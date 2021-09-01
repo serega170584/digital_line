2f366da1e04418ee7e729cc400bc315b69d1baa3
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-01 15:43:07 +0300

    controller group generator add

--- src/Controller/CompetitionController.php
+++ src/Controller/CompetitionController.php
@@ -2,10 +2,10 @@
 
 namespace App\Controller;
 
+use App\Domain\GroupGenerator;
 use App\Repository\StageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class CompetitionController extends AbstractController
 {
@@ -19,5 +19,15 @@ class CompetitionController extends AbstractController
         return $this->render('competition/index.html.twig', [
             'controller_name' => 'CompetitionController',
         ]);
+    }
+
+    /**
+     * @Route("/table", name="table")
+     * @param GroupGenerator $generator
+     * @return Response
+     */
+    public function table(GroupGenerator $generator): Response
+    {
+        $generator->generate();
     }
 }
