a74528ea63374d69af87b04fceb892731f95e1fc
Sergey Krivobokov <serega170584@gmail.com>
2021-09-01 09:04:39 +0300

    Stage migration create

---
+++ src/Entity/Stage.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StageRepository::class)
+ */
+class Stage
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $stageOrder;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isPlayoff;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStageOrder(): ?string
+    {
+        return $this->stageOrder;
+    }
+
+    public function setStageOrder(string $stageOrder): self
+    {
+        $this->stageOrder = $stageOrder;
+
+        return $this;
+    }
+
+    public function getIsPlayoff(): ?bool
+    {
+        return $this->isPlayoff;
+    }
+
+    public function setIsPlayoff(bool $isPlayoff): self
+    {
+        $this->isPlayoff = $isPlayoff;
+
+        return $this;
+    }
+}
