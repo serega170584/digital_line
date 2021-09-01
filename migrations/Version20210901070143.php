234161f22c56251c68b3fd733dd2e160502fea03
Sergey Krivobokov <serega170584@gmail.com>
2021-09-01 14:46:58 +0300

    Group entity add

---
+++ migrations/Version20210901070143.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210901070143 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE play ADD scored_goals INT NOT NULL, ADD lost_goals INT NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE play DROP scored_goals, DROP lost_goals');
+    }
+}
