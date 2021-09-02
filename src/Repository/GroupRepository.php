d093baa206796a5cc838f1b9c84d46240b26d5df
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-02 15:27:51 +0300

    generators refactoring

--- src/Repository/GroupRepository.php
+++ src/Repository/GroupRepository.php
@@ -92,7 +92,7 @@ class GroupRepository extends ServiceEntityRepository implements RepositoryInter
     public function findGroups()
     {
         return $this->createQueryBuilder('g')
-            ->innerJoin(Team::class, 't', Join::WITH, 'g.id = t.team_group_id')
+            ->innerJoin(Team::class, 't', Join::WITH, 'g.id = t.teamGroup')
             ->innerJoin(Play::class, 'p', Join::WITH, 'p.teamId = t.id')
             ->orderBy('g.id', 'ASC')
             ->addOrderBy('t.points', 'DESC')
