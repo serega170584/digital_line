3a474ab8ca59d38feffbb2e5abfae88c724cef67
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-03 15:32:32 +0300

    grid view fix

--- src/Repository/GroupRepository.php
+++ src/Repository/GroupRepository.php
@@ -97,7 +97,7 @@ class GroupRepository extends ServiceEntityRepository implements RepositoryInter
             ->innerJoin(Team::class, 't', Join::WITH, 'g.id = t.teamGroup')
             ->innerJoin(Play::class, 'p', Join::WITH, 'p.team = t.id')
             ->innerJoin(Stage::class, 's', Join::WITH, 'p.stage = s.id')
-            ->where('s.isPlayoff=')
+            ->where('s.isPlayoff=1')
             ->orderBy('g.id', 'ASC')
             ->getQuery()
             ->getResult();
