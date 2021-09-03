167f8fbde23a9e304d9c227de5120a8297ad3025
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-03 09:38:42 +0300

    playoff generator check

--- src/Domain/Generators/PlayOffStageGenerator.php
+++ src/Domain/Generators/PlayOffStageGenerator.php
@@ -9,9 +9,7 @@ class PlayOffStageGenerator extends Generator
 
     public function generate(): self
     {
-        $this->records = [['1/4', true]];
-        $this->records = [['1/2', true]];
-        $this->records = [['Final', true]];
+        $this->records = [['1/4', true], ['1/2', true], ['Final', true]];
         return $this;
     }
 }
