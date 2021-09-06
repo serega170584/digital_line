38b18f89c614e9783b3f89d4d5a3396db0f14b29
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-06 06:56:25 +0300

    generator refactoring

--- src/Repository/StageRepository.php
+++ src/Repository/StageRepository.php
@@ -6,6 +6,7 @@ use App\Domain\Collections\StageArrayCollection;
 use App\Entity\Stage;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Laminas\Code\Generator\GeneratorInterface;
 
 /**
  * @method Stage|null find($id, $lockMode = null, $lockVersion = null)
