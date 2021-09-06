0021d334606aa545094952de6767eb8c0e47c42f
Sergey Krivobokov <serega170584@gmail.com>
2021-09-06 09:18:39 +0300

    Migration add

---
+++ migrations/Version20210906061814.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210906061814 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE INDEX stage_order_id_idx ON play (stage_order)');
+        $this->addSql('CREATE UNIQUE INDEX team_opponent_idx ON play (team_id, opponent_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX stage_order_id_idx ON play');
+        $this->addSql('DROP INDEX team_opponent_idx ON play');
+    }
+}
