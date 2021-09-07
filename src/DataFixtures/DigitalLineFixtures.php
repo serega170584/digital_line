153366cf11b2441c7bee9c5308447bc95592e3fc
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-07 14:39:16 +0300

    play generator refactoring

--- src/DataFixtures/DigitalLineFixtures.php
+++ src/DataFixtures/DigitalLineFixtures.php
@@ -3,10 +3,6 @@
 namespace App\DataFixtures;
 
 use App\Domain\Generator\CompetitionGenerator;
-use App\Domain\Generator\GroupGenerator;
-use App\Domain\Generator\PlayGenerator;
-use App\Domain\Generator\StageGenerator;
-use App\Domain\Generator\TeamGenerator;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -19,19 +15,10 @@ class DigitalLineFixtures extends Fixture
      */
     public function load(ObjectManager $manager)
     {
-        $stageGenerator = new StageGenerator(null);
-        $groupGenerator = new GroupGenerator(null);
-        $teamGenerator = new TeamGenerator(null);
-        $playGenerator = new PlayGenerator(null);
         /**
          * @var ObjectManager|EntityManagerInterface $manager
          */
-        $generator = new CompetitionGenerator(null,
-            $stageGenerator,
-            $groupGenerator,
-            $teamGenerator,
-            $playGenerator
-        );
+        $generator = new CompetitionGenerator(null);
         $generator->setManager($manager);
         $generator->execute();
     }
