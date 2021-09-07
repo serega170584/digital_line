65dc1b53a2afcf5e1efce59b41c00c9fb028128e
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-07 15:22:54 +0300

    play generator refactoring

---
+++ tests/Domain/Generator/StageGeneratorTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Tests\Service;
+
+
+use App\Entity\Group;
+use App\Repository\StageRepository;
+use Doctrine\Common\Collections\Criteria;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class StageGeneratorTest extends KernelTestCase
+{
+    public function testGenerate()
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        /**
+         * @var StageRepository $stageRepository
+         */
+        $stageRepository = $container->get(StageRepository::class);
+        /**
+         * @var Stage[] $stages
+         */
+        $stages = $stageRepository->findBy([], [self::ID => Criteria::ASC]);
+        $names = array_map(function (Group $group) {
+            return $group->getName();
+        }, $stages);
+        $this->assertEqualsCanonicalizing($names, [
+            'Preliminary round',
+            '1/4',
+            '1/2',
+            'Final'
+        ]);
+    }
+}
