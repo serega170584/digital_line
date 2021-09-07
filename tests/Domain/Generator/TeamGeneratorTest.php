33daa3788be21bfc79fb4dd7132ca5f0da5f4c08
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-07 15:46:43 +0300

    stage unit test fix

--- tests/Domain/Generator/TeamGeneratorTest.php
+++ tests/Domain/Generator/TeamGeneratorTest.php
@@ -59,5 +59,21 @@ class TeamGeneratorTest extends KernelTestCase
             'A', 'B', 'C', 'D',
             'E', 'F', 'G', 'H',
         ]);
+
+        /**
+         * @var Group $group
+         */
+        $group = current($groupRepository->findBy([self::NAME => 'B'], [self::ID => Criteria::ASC]));
+        /**
+         * @var Team[] $stages
+         */
+        $teams = $teamRepository->findBy([self::GROUP => $group], [self::ID => Criteria::ASC]);
+        $names = array_map(function (Team $team) {
+            return $team->getName();
+        }, $teams);
+        $this->assertEqualsCanonicalizing($names, [
+            'I', 'J', 'K', 'L',
+            'M', 'N', 'O', 'P'
+        ]);
     }
 }
