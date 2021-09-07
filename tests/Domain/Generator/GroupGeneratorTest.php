0b68fdb3b9c2e832aeefd03faaba9fa5488ff84a
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-07 15:09:28 +0300

    play generator refactoring

--- tests/Domain/Generator/GroupGeneratorTest.php
+++ tests/Domain/Generator/GroupGeneratorTest.php
@@ -26,7 +26,7 @@ class GroupGeneratorTest extends KernelTestCase
         $names = array_map(function (Group $group) {
             return $group->getName();
         }, $groups);
-        var_dump($names);
+        $this->assertEquals($names, $names);
         $this->assertEquals(['A', 'B'], $names);
     }
 }
