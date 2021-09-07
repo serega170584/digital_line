9ff5dc575f8bd3da522f4c6fff2f35ca932c51f8
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-07 15:06:24 +0300

    play generator refactoring

---
+++ tests/Domain/Generator/GroupGeneratorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Entity\Group;
+use App\Repository\GroupRepository;
+use Doctrine\Common\Collections\Criteria;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class GroupGeneratorTest extends KernelTestCase
+{
+    const ID = 'id';
+
+    public function testGenerate()
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        /**
+         * @var GroupRepository $groupRepository
+         */
+        $groupRepository = $container->get(GroupRepository::class);
+        /**
+         * @var Group[] $groups
+         */
+        $groups = $groupRepository->findBy([self::ID => Criteria::ASC]);
+        $names = array_map(function (Group $group) {
+            return $group->getName();
+        }, $groups);
+        $this->assertEquals(['A', 'B'], $names);
+    }
+}
