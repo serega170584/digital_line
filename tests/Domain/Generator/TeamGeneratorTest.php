3b5f7bda46d0b412203e211f0875a118ac73c5c4
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-07 15:45:20 +0300

    stage unit test fix

--- tests/Domain/Generator/TeamGeneratorTest.php
+++ tests/Domain/Generator/TeamGeneratorTest.php
@@ -3,7 +3,9 @@
 namespace App\Tests\Service;
 
 
+use App\Entity\Group;
 use App\Entity\Team;
+use App\Repository\GroupRepository;
 use App\Repository\TeamRepository;
 use Doctrine\Common\Collections\Criteria;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -11,6 +13,8 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 class TeamGeneratorTest extends KernelTestCase
 {
     const ID = 'id';
+    const NAME = 'name';
+    const GROUP = 'group';
 
     public function testGenerate()
     {
@@ -33,6 +37,27 @@ class TeamGeneratorTest extends KernelTestCase
             'E', 'F', 'G', 'H',
             'I', 'J', 'K', 'L',
             'M', 'N', 'O', 'P'
+        ]);
+
+        /**
+         * @var GroupRepository $groupRepository
+         */
+        $groupRepository = $container->get(GroupRepository::class);
+
+        /**
+         * @var Group $group
+         */
+        $group = current($groupRepository->findBy([self::NAME => 'A'], [self::ID => Criteria::ASC]));
+        /**
+         * @var Team[] $stages
+         */
+        $teams = $teamRepository->findBy([self::GROUP =>$group], [self::ID => Criteria::ASC]);
+        $names = array_map(function (Team $team) {
+            return $team->getName();
+        }, $teams);
+        $this->assertEqualsCanonicalizing($names, [
+            'A', 'B', 'C', 'D',
+            'E', 'F', 'G', 'H',
         ]);
     }
 }
