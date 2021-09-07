6c970d5a8d0e6c3df40eec24d5b6d50b86d13ad6
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-07 15:13:03 +0300

    play generator refactoring

--- tests/Domain/Generator/GroupGeneratorTest.php
+++ tests/Domain/Generator/GroupGeneratorTest.php
@@ -22,7 +22,7 @@ class GroupGeneratorTest extends KernelTestCase
         /**
          * @var Group[] $groups
          */
-        $groups = $groupRepository->findBy([self::ID => Criteria::ASC]);
+        $groups = $groupRepository->findAll();
         var_dump(count($groups));
         $names = array_map(function (Group $group) {
             return $group->getName();
