092c541b42dc8c64bc5a70ed9a94df00a08b47b4
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-08 09:06:58 +0300

    tournament refactoring

--- tests/Domain/Tournaments/PlayoffTournamentTest.php
+++ tests/Domain/Tournaments/PlayoffTournamentTest.php
@@ -49,5 +49,25 @@ class PlayoffTournamentTest extends KernelTestCase
             return $play->getLostGoals();
         });
         $this->assertEquals([0, 0, 0, 0], array_values($lostGoals->toArray()));
+
+        $stage = $stages->next();
+        $this->assertEquals('1/4', $stage->getName());
+        $plays = $stage->getOrderedPlays();
+        $teams = $plays->map(function (Play $play) {
+            return $play->getTeam()->getName();
+        });
+        $this->assertEquals(['A', 'C'], array_values($teams->toArray()));
+        $opponents = $plays->map(function (Play $play) {
+            return $play->getOpponent()->getName();
+        });
+        $this->assertEquals(['B', 'D'], array_values($opponents->toArray()));
+        $scoredGoals = $plays->map(function (Play $play) {
+            return $play->getScoredGoals();
+        });
+        $this->assertEquals([1, 1], array_values($scoredGoals->toArray()));
+        $lostGoals = $plays->map(function (Play $play) {
+            return $play->getLostGoals();
+        });
+        $this->assertEquals([0, 0], array_values($lostGoals->toArray()));
     }
 }
