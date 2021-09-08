f110d17c7aa12c0c8d4666f739719de1ec3b6e9a
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-08 08:58:15 +0300

    tournament refactoring

---
+++ tests/Domain/Tournaments/PlayoffTournamentTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Domain\Tournaments\PlayoffTournament;
+use App\Entity\Stage;
+use App\Repository\StageRepository;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class PlayoffTournamentTest extends KernelTestCase
+{
+    public function testBuild()
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        /**
+         * @var StageRepository $stageRepository
+         */
+        $stageRepository = $container->get(StageRepository::class);
+        $stages = $stageRepository->findAllArrayCollection();
+        $playoffStages = $stages->findIsPlayoff();
+        /**
+         * @var PlayoffTournament $playoffTournament
+         */
+        $playoffTournament = $container->get(PlayoffTournament::class);
+        $playoffTournament->setStages($playoffStages);
+        $playoffTournament->build();
+        $stages = $playoffTournament->getStages();
+        /**
+         * @var Stage $stage
+         */
+        $stage = $stages->current();
+        $this->assertEquals('1/4', $stage->getName());
+    }
+}
