b89c41946827d5f288d008baf5e03084f56b5510
Sergey Krivobokov <krivobokov.sv@academia-moscow.ru>
2021-09-08 08:30:23 +0300

    tournament refactoring

---
+++ tests/Domain/Tournaments/GroupTournamentTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Domain\Tournaments\GroupTournament;
+use App\Entity\Play;
+use App\Entity\Team;
+use App\Repository\TeamRepository;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class GroupTournamentTest extends KernelTestCase
+{
+    public function testBuild()
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        /**
+         * @var GroupTournament $groupTournament
+         */
+        $groupTournament = $container->get(GroupTournament::class);
+        $groupTournament->build();
+        /**
+         * @var TeamRepository $teamRepository
+         */
+        $teamRepository = $container->get(TeamRepository::class);
+        $teams = $teamRepository->findAll();
+        $team = current($teams);
+        $plays = $groupTournament->findTeamPlays($team);
+        $teamNames = $plays->map(function (Play $play) {
+            return $play->getTeam()->getName();
+        });
+        $this->assertEquals(['A', 'A', 'A', 'A', 'A', 'A', 'A', 'A'], $teamNames->toArray());
+        $opponentNames = $plays->map(function (Play $play) {
+            return $play->getOpponent()->getName();
+        });
+        $this->assertEquals(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'], $opponentNames->toArray());
+        $scoredGoals = $plays->map(function (Play $play) {
+            return $play->getScoredGoals();
+        });
+        $this->assertEquals([0, 1, 1, 1, 1, 1, 1, 1], $scoredGoals->toArray());
+        $lostGoals = $plays->map(function (Play $play) {
+            return $play->getLostGoals();
+        });
+        $this->assertEquals([0, 0, 0, 0, 0, 0, 0, 0], $lostGoals->toArray());
+    }
+}
